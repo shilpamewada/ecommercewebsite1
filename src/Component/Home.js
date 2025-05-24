e5606939ea8d09e440a6c54532738b9133c03f91
Shilpa Md <shilpa@example.com>
2025-05-24 15:50:31 +0530

    added

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -8,13 +8,10 @@ function Home({ addToCart }) {
   return (
     <div className='hero'>
       <div className="card text-bg-dark text-white border-0">
-        <img src="/assets/bg.jpg" className="card-img" alt="Background" height="550px" />
+        <img src="/assets/pexels-cottonbro-6069978.jpg" className="card-img" alt="Background" />
         <div className="card-img-overlay d-flex flex-column justify-content-center ">
           <div className="container">
             <h5 className="card-title  display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
-            <p className="card-text">
-              CHECK OUT ALL THE TRENDS
-            </p>
           </div>
         </div>
       </div>
