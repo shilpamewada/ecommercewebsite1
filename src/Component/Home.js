9284ad457d1c54bd3663f929d449a8f68460e3ff
shilpamewada <156938495+shilpamewada@users.noreply.github.com>
2025-12-10 13:10:33 +0000

    add my image

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -7,7 +7,7 @@ function Home({ addToCart }) {
   return (
     <div className='hero'>
       <div className="card text-bg-dark text-white border-0">
-        <img src="/assets/pexels-cottonbro-6069978.jpg" className="card-img" alt="Background" />
+        <img src="/assets/bg.jpg" className="card-img" alt="Background" />
         <div className="card-img-overlay d-flex flex-column justify-content-center ">
           <div className="container">
             <h5 className="card-title  display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
