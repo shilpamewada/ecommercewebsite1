78b4b6a27f9b3881b845fe19b37b48a13ebd1cd1
shilpamewada <156938495+shilpamewada@users.noreply.github.com>
2025-12-10 13:14:03 +0000

    project is done

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -2,12 +2,13 @@
 import Products from './Products';
 import Contact from './Contact';
 import Images from './Images';
+import bgjpg from './bg.jpg';
 
 function Home({ addToCart }) {
   return (
     <div className='hero'>
       <div className="card text-bg-dark text-white border-0">
-        <img src="/assets/bg.jpg" className="card-img" alt="Background" />
+        <img src={bgjpg} className="card-img" alt="Background" />
         <div className="card-img-overlay d-flex flex-column justify-content-center ">
           <div className="container">
             <h5 className="card-title  display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
